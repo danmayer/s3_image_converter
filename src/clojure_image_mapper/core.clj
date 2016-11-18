c69492d63934bc27a4aa178fd122685c06d7f1bc
Dan Mayer <dan.mayer@offgrid-electric.com>
2016-11-17 22:53:46 -0700

    writes and rotates... some files are empty which crashes the rotate

--- src/clojure_image_mapper/core.clj
+++ src/clojure_image_mapper/core.clj
@@ -7,8 +7,16 @@
             [fivetonine.collage.core :refer :all]
             [clojure.java.io :as io]))
 
-;;todo move to ~/.aws... needs file expansion
-(def aws-creds (slurp "/Users/danmayer/.aws/credentials"))
+
+(defn expand-home [s]
+  (if (.startsWith s "~")
+    (string/replace-first s "~" (System/getProperty "user.home"))
+    s))
+
+(def aws-creds (slurp (expand-home "~/.aws/credentials")))
+
+;;todo pass in bucketname
+(def bucket-name "offgridelectricdev")
 
 (def aws-access-key-id
   (nth (re-find #"(?m)^aws_access_key_id.*=\s(.*)", aws-creds) 1)
@@ -22,75 +30,81 @@
 
 (defn entry-list [cred, bucket] (map :key (get (s3/list-objects cred bucket) :objects)))
 
-(defn filtered-image-paths [entry-list] (filter #(re-find #"\.jpg" %) entry-list))
+(defn filtered-image-paths [entry-list]
+  (remove #(re-find #"thumb" %)
+    (filter #(re-find #"\.jpg" %) entry-list))
+  )
 
-(defn convert-image [image-path]
-  (println image-path)
-  (let [converted-path (string/replace image-path #"\.jpg" ".webp")]
-     (println converted-path)
-     (with-image image-path
-            (resize :width 100)
-            (rotate 90)
-            (util/save converted-path :quality 0.85))
-     converted-path
+(defn write-to-s3 [cred bucket image-path local-path]
+  (let [converted-path (string/replace image-path #"\.jpg" "_dan_test.jpg")]
+    (println "************************")
+    (println converted-path)
+    (println "************************")
+    (s3/put-object cred bucket converted-path (io/file local-path)
+                :content-type "image/jpg")
+    converted-path
   )
 )
 
-(defn write-to-s3 [cred image-path local-path]
-  (reduce + (range 0 9000000))
-  (let [converted-path (string/replace image-path #"\.jpg" ".webp")]
+(defn convert-image [image-path local-path]
+  (let [converted-path (string/replace local-path #"\.jpg" "_dan_test.jpg")]
      (println converted-path)
-     converted-path
+     (with-image local-path
+            (resize :width 100)
+            (rotate 90)
+            (util/save converted-path :quality 0.85))
+     [image-path converted-path]
   )
 )
 
 (defn read-from-s3 [cred bucket image-path]
   (let [local-path (string/join "/" ["/tmp", image-path])]
+    (println local-path)
     (clojure.java.io/make-parents local-path)
-    (println (class (:content (s3/get-object cred bucket image-path))))
     (io/copy
      (:content (s3/get-object cred bucket image-path))
      (io/output-stream local-path))
-    local-path))
+    [image-path local-path]))
 
 ;;add method to clean up temp files
 
 (defn -main[]
-  (let [ch1 (async/chan 500)
-       ch2 (async/chan 500)
+  (let [ch1 (async/chan 8)
+       ch2 (async/chan 8)
+       ch3 (async/chan 8)
+       ch4 (async/chan 8)
        exitchan (async/chan)]
 
     (async/thread
        (loop[]
-         (when-let [path (async/<!! ch2)]
-           (println path)
+         (when-let [path (async/<!! ch4)]
+           ;;(println path)
            (recur)
          )
        )
        (async/close! exitchan)
     )
 
-;;    (async/pipeline-async 8 ch2 (fn [path c]
-;;        (async/>!! c (write-to-s3 cred path "batman"))
-;;        (async/close! c)
-;;      ) ch1
-;;    )
+   (async/pipeline-async 8 ch4 (fn [[image-path local-path] c]
+       (async/>!! c (write-to-s3 cred bucket-name image-path local-path))
+       (async/close! c)
+     ) ch3
+   )
 
-;;    (async/pipeline-async 8 ch2 (fn [path c]
-;;        (async/>!! c (convert-image cred path "batman"))
-;;        (async/close! c)
-;;      ) ch1
-;;    )
+    (async/pipeline-async 8 ch3 (fn [[image-path local-path] c]
+       (async/>!! c (convert-image image-path local-path))
+       (async/close! c)
+     ) ch2
+    )
 
     (async/pipeline-async 8 ch2 (fn [path c]
-        (async/>!! c (read-from-s3 cred "offgridelectricdev" path))
+        (async/>!! c (read-from-s3 cred bucket-name path))
         (async/close! c)
       ) ch1
     )
 
-    ;;todo pass in bucketname
     (doseq [path (->>
-                 (entry-list cred "offgridelectricdev")
+                 (entry-list cred bucket-name)
                  (filtered-image-paths))]
          (async/>!! ch1 path))
     (async/close! ch1)
