7b1db86325e59f48429b79dc45cc87db49829d38
Ben Brinckerhoff <ben@bbrinck.com>
2016-11-19 13:06:17 -0700

    Clean up some ambigous version ranges

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-aws-s3 "0.3.10"]
+                 ;; avoid warning about version ranges
+                 [clj-aws-s3 "0.3.10" :exclusions [commons-codec joda-time]]
+                 [joda-time "2.2"]
                  [org.clojure/core.async "0.2.395"]
-                 [fivetonine/collage "0.2.1"]])
-                 
+                 [fivetonine/collage "0.2.1" ]])
