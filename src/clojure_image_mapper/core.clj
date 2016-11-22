c989770da32f9b0545458c24201b776c8d33e943
Dan Mayer <dan.mayer@offgrid-electric.com>
2016-11-21 15:43:04 -1000

    it WORKS, now time to clean this up

--- src/clojure_image_mapper/core.clj
+++ src/clojure_image_mapper/core.clj
@@ -5,11 +5,19 @@
             [clojure.string :as string]
             [fivetonine.collage.util :as util]
             [fivetonine.collage.core :refer :all]
-            [clojure.java.io :as io])
-  ;; Temporary so I can try to figure out webp support
-  (:import
-    javax.imageio.ImageIO
-    )
+            [clojure.java.io :as io]
+            [clojure.java.io :refer [as-file file]])
+  ;; needed to override collage method
+   (:import java.io.File
+           java.net.URI
+           java.net.URL
+           java.awt.image.BufferedImage
+           javax.imageio.ImageIO
+           javax.imageio.IIOImage
+           javax.imageio.ImageWriter
+           javax.imageio.ImageWriteParam
+           com.luciad.imageio.webp.WebPWriteParam
+           fivetonine.collage.Frame)
   )
 
 
@@ -96,6 +104,52 @@
 
 ;;add method to clean up temp files
 
+;; attemping to overrie
+
+(declare parse-extension)
+
+(defn parse-extension
+  "Parses the image extension from the path."
+  [path]
+  (last (clojure.string/split path #"\.")))
+
+(defn save_webp
+  "Store an image on disk.
+  Accepts optional keyword arguments.
+  `:quality` - decimal, between 0.0 and 1.0. Defaults to 0.8.
+  `:progressive` - boolean, `true` turns progressive saving on, `false`
+  turns it off. Defaults to the default value in the ImageIO API -
+  `ImageWriteParam/MODE_COPY_FROM_METADATA`. See
+  [Java docs](http://docs.oracle.com/javase/7/docs/api/javax/imageio/ImageWriteParam.html).
+  Examples:
+    (save image \"/path/to/new/image.jpg\" :quality 1.0)
+    (save image \"/path/to/new/image/jpg\" :progressive false)
+  Returns the path to the saved image when saved successfully."
+  [^BufferedImage image path & rest]
+  (let [opts (apply hash-map rest)
+        outfile (file path)
+        ext (parse-extension path)
+        ^ImageWriter writer (.next (ImageIO/getImageWritersByFormatName ext))
+        ^ImageWriteParam write-param (.getDefaultWriteParam writer)
+        iioimage (IIOImage. image nil nil)
+        outstream (ImageIO/createImageOutputStream outfile)]
+    (doto write-param
+      (.setCompressionType "Lossless"))
+    (when (.canWriteProgressive write-param)
+      (let [mode-map {true  ImageWriteParam/MODE_DEFAULT
+                      false ImageWriteParam/MODE_DISABLED}
+            mode-flag (get opts :progressive)]
+        (doto write-param
+          (.setProgressiveMode (get mode-map
+                                    mode-flag
+                                    ImageWriteParam/MODE_COPY_FROM_METADATA)))))
+    (doto writer
+      (.setOutput outstream)
+      (.write nil iioimage write-param)
+      (.dispose))
+    (.close outstream)
+    path))
+
 (defn -main[]
   (let [ch1 (async/chan 8)
        ch2 (async/chan 8)
@@ -106,9 +160,12 @@
 
   (println (into [] (ImageIO/getReaderFormatNames)))
 
-  ;; this throws an exception
-  (with-image "/tmp/agent_profiles/1000/house_images/device-3ae02e86cdab49eb0f42a7ca561795e6.jpg"
-    (util/save "/tmp/agent_profiles/1000/house_images/device-3ae02e86cdab49eb0f42a7ca561795e6.webp"))
+
+    ;; this throws an exception
+    (with-image "/tmp/agent_profiles/1000/house_images/device-3ae02e86cdab49eb0f42a7ca561795e6.jpg"
+      (save_webp "/tmp/agent_profiles/1000/house_images/device-3ae02e86cdab49eb0f42a7ca561795e6.webp" :quality 0.8))
+
+    (System/exit 0)
 
     (async/thread
        (loop[]
